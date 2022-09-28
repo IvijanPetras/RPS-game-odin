44a08cda1dcaa24ab5f082daa21de6fb278c6e7b
Ivijan <iapetras@hotmail.com>
2022-09-28 07:55:23 +0200

    added visuals

--- script.js
+++ script.js
@@ -1,62 +1,108 @@
 addEventListener("DOMContentLoaded", () => {
+  var decision = "";
+  let plScore = 0;
+  let pcScore = 0;
+  let player = document.querySelector(".player");
+  let pc = document.querySelector(".pc");
+
+  const rock = document.querySelector(".rock");
+  const paper = document.querySelector(".paper");
+  const scissors = document.querySelector(".scissors");
+  const choice = document.querySelector(".rock2");
+  const choice2 = document.querySelector(".paper2");
+  const choice3 = document.querySelector(".scissors2");
+
+  function winner() {
+    if (plScore > 2) {
+      alert("Player wins!");
+      plScore = 0;
+      pcScore = 0;
+      player.innerHTML = "Player: 0";
+      pc.innerHTML = "PC: 0";
+    } else if (pcScore > 2) {
+      alert("PC Wins!");
+      plScore = 0;
+      pcScore = 0;
+      player.innerHTML = "Player: 0";
+      pc.innerHTML = "PC: 0";
+    }
+  }
+
   function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3);
     switch (randomNum) {
       case 0:
+        choice.classList.add("img-clicked");
+        removeClass(choice);
         return "ROCK";
         break;
       case 1:
+        choice2.classList.add("img-clicked");
+        removeClass(choice2);
         return "PAPER";
         break;
       case 2:
+        choice3.classList.add("img-clicked");
+        removeClass(choice3);
         return "SCISSORS";
     }
   }
 
-  function playRound(computerChoice) {
-    let player = prompt("whats your choice? Spell it corectly!").toUpperCase();
-    if (player === computerChoice) {
+  function removeClass(e) {
+    setTimeout(() => e.classList.remove("img-clicked"), 1000);
+  }
+
+  function playRound(dec) {
+    let pcChoice = getComputerChoice();
+    if (dec === pcChoice) {
       console.log("Its A Tie!");
-      return 2;
-    } else if (player === "ROCK" && computerChoice === "PAPER") {
+      return;
+    } else if (dec === "ROCK" && pcChoice === "PAPER") {
       console.log("Paper Beats Rock! PC Wins!");
-      return 0;
-    } else if (player === "ROCK" && computerChoice === "SCISSORS") {
+      pcScore++;
+      pc.innerHTML = `PC: ${pcScore}`;
+    } else if (dec === "ROCK" && pcChoice === "SCISSORS") {
       console.log("Rock Beats Scissors! Player Wins!");
-      return 1;
-    } else if (player === "PAPER" && computerChoice === "ROCK") {
+      plScore++;
+      player.innerHTML = `Player: ${plScore}`;
+    } else if (dec === "PAPER" && pcChoice === "ROCK") {
       console.log("Paper Beats Rock! Player Wins!");
-      return 1;
-    } else if (player === "PAPER" && computerChoice === "SCISSORS") {
+      plScore++;
+      player.innerHTML = `Player: ${plScore}`;
+    } else if (dec === "PAPER" && pcChoice === "SCISSORS") {
       console.log("Scissors Beat Paper! PC Wins!");
-      return 0;
-    } else if (player === "SCISSORS" && computerChoice === "ROCK") {
+      pcScore++;
+      pc.innerHTML = `PC: ${pcScore}`;
+    } else if (dec === "SCISSORS" && pcChoice === "ROCK") {
       console.log("Rock Beats Scissors! PC Wins!");
-      return 0;
-    } else if (player === "SCISSORS" && computerChoice === "PAPER") {
+      pcScore++;
+      pc.innerHTML = `PC: ${pcScore}`;
+    } else if (dec === "SCISSORS" && pcChoice === "PAPER") {
       console.log("Scissors Beat Paper! Player Wins!");
-      return 1;
+      plScore++;
+      player.innerHTML = `Player: ${plScore}`;
     }
   }
 
-  function game() {
-    let plScore = 0;
-    let pcScore = 0;
-    for (let i = 0; i < 5; i++) {
-      let rnd = playRound(getComputerChoice());
-      if (rnd == 1) {
-        plScore++;
-      } else if (rnd == 2) {
-        i--;
-      } else {
-        pcScore++;
-      }
-    }
-    if (plScore > pcScore) {
-      return alert("Player Wins!");
-    }
-    return alert("Pc Wins!");
-  }
-
-  console.log(game());
+  rock.onclick = () => {
+    decision = "ROCK";
+    rock.classList.add("img-clicked");
+    playRound(decision);
+    removeClass(rock);
+    setTimeout(() => winner(), 1000);
+  };
+  paper.onclick = () => {
+    decision = "PAPER";
+    paper.classList.add("img-clicked");
+    playRound(decision);
+    removeClass(paper);
+    setTimeout(() => winner(), 1000);
+  };
+  scissors.onclick = () => {
+    decision = "SCISSORS";
+    scissors.classList.add("img-clicked");
+    playRound(decision);
+    removeClass(scissors);
+    setTimeout(() => winner(), 1000);
+  };
 });
